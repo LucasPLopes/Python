6ae7e68568e7ade2f5a3644197d5b2decebebc76
lucas <l.lopes.fga@gmail.com>
2019-07-20 16:30:59 -0300

    evolucao da classe Thief

--- TH_house/oop/01/characteres.py
+++ TH_house/oop/01/characteres.py
@@ -3,12 +3,21 @@ from random import randint
 class Thief:#obj = Thief()
     sneaky = True
     
-    def pickPocket(self):#Thief.pickpocket(gatuno) tb
+    def pickPocket(self):#Thief.pickPocket(gatuno) tb
         print("Called by {}".format(self))
-        if(sneaky):
+        if self.sneaky:
             return bool(randint(0,1))
         return False
 
+class Thief2(object):
+    sneaky: True    #sorrateiro
+    
+    def pickPocket(self):
+        if self.sneaky:
+            return bool(randint(0,1))
+        return False
 
+    def hide(self, light_level):
+        return self.sneaky and light_level <= 10 
 
             
