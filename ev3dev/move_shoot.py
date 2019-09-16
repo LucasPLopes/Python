9e6f14e04516b1c8dabe4eaaa86a6efe1ddc0612
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-16 13:43:45 -0300

    Updates

--- ev3dev/move_shoot.py
+++ ev3dev/move_shoot.py
@@ -6,13 +6,28 @@ from time import sleep
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 gun = MediumMotor(OUTPUT_D)
 def goforwad(robo, rotations):
-    for i in range(1,rotations):
-        robo.on_for_rotations(SpeedPercent(100), SpeedPercent(100), i)
-        time.sleep(i)
+    robo.on_for_rotations(SpeedPercent(100), SpeedPercent(100), rotations)
+
+def goback(robo,rotations):
+    robo.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), rotations)
 
 def shoot(robo):
     robo.on_for_rotations(SpeedPercent(100),1)
     robo.on_for_rotations(SpeedPercent(100),-1)
 
+def goleft(robo,rotations):
+    robo.on_for_rotations(SpeedPercent(30), SpeedPercent(60), rotations)
+
+def goright(robo,rotations):
+    robo.on_for_rotations(SpeedPercent(60), SpeedPercent(30), rotations)
+
+def gobleft(robo,rotations):
+    robo.on_for_rotations(SpeedPercent(-30), SpeedPercent(-60), rotations)
+
+def gobright(robo,rotations):
+    robo.on_for_rotations(SpeedPercent(-60), SpeedPercent(-30), rotations)
+
+def go180(robo):
+    robo.on_for_rotations(SpeedPercent(-100), SpeedPercent(100), 5  )
 goforwad(tank_drive, 3)
 shoot(gun)
