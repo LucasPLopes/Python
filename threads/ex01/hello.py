1240ebdff23ffbd1ff0351953043ff3dc837775e
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-16 11:54:52 -0300

    Hello using thread

---
+++ threads/ex01/hello.py
@@ -0,0 +1,10 @@
+import threading 
+import time
+
+def worker(message):
+    for i in range(5):
+        print(message)
+        time.sleep(1)
+
+t = threading.Thread(target=worker, args=("Thread sendo executada",))
+t.start()
